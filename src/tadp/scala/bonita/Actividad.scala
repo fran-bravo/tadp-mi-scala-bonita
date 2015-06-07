42a46959426fe4f2ac7c702720ae3d819df2a28e
Dario Cappellini <dcappellini@hotmail.com.ar>
2015-06-07 02:10:10 -0300

    Agrego metodo abstracto realizar para el trait Actividad.

--- src/tadp/scala/bonita/Actividad.scala
+++ src/tadp/scala/bonita/Actividad.scala
@@ -3,6 +3,13 @@ package tadp.scala.bonita
 /**
  * @author Dario
  */
-class Actividad {
+trait Actividad {
+  def realizar(pokemon: Pokemon) : Pokemon = pokemon.estado match {
+    case KO => throw new KOException
+    case Dormido => pokemon //????? FALTA IMPLEMENTAR LO DE QUE SE VA DESPERTANDO
+    case poke => doRealizar(pokemon)
+  }
   
+  def doRealizar(pokemon:Pokemon) : Pokemon
 }
+
