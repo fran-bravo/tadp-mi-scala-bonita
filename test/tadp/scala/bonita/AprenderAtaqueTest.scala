e33d30935624eec610873b394c54c3c83e5e6d4b
Dario Cappellini <dcappellini@hotmail.com.ar>
2015-06-09 11:34:48 -0300

    Más tests para aprender ataque, y manejo de PP.

--- test/tadp/scala/bonita/AprenderAtaqueTest.scala
+++ test/tadp/scala/bonita/AprenderAtaqueTest.scala
@@ -16,5 +16,36 @@ class AprenderAtaqueTest {
     Assert.assertTrue(pikachu.sabeElAtaque(fixture.thunderbolt))
   }
   
+  @Test
+  def `pokemon electrico aprende thunderbolt y obtiene 15 pp`
+  {
+    var pikachu = fixture.nuevoPikachuM()
+    pikachu = pikachu.realizarActividad(AprenderAtaque(fixture.thunderbolt))
+    Assert.assertEquals(15, pikachu.paActual(fixture.thunderbolt))
+  }
+  
+  @Test
+  def `pokemon electrico aprender thunderbolt y obtiene 15 pp max`
+  {
+    var pikachu = fixture.nuevoPikachuM()
+    pikachu = pikachu.realizarActividad(AprenderAtaque(fixture.thunderbolt))
+    Assert.assertEquals(15, pikachu.paMax(fixture.thunderbolt))
+  }
+  
+  @Test
+  def `pokemon electrico aprende thunderbolt y lo usa, decrementa su pp actual pero no el max`
+  {
+    var pikachu = fixture.nuevoPikachuM()
+    pikachu = pikachu.realizarActividad(AprenderAtaque(fixture.thunderbolt))
+    Assert.assertEquals(15, pikachu.paActual(fixture.thunderbolt))
+    Assert.assertEquals(15, pikachu.paMax(fixture.thunderbolt))
+    pikachu = pikachu.realizarActividad(RealizarAtaque(fixture.thunderbolt))
+    pikachu = pikachu.realizarActividad(RealizarAtaque(fixture.thunderbolt))
+    pikachu = pikachu.realizarActividad(RealizarAtaque(fixture.thunderbolt))
+    Assert.assertEquals(12, pikachu.paActual(fixture.thunderbolt))
+    Assert.assertEquals(15, pikachu.paMax(fixture.thunderbolt))
+    
+  }
+  
   
 }
